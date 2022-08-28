e9075d95ae00e846df29ecdd18453132911917af
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-08-28 11:41:58 -0500

    Basic data variables

---
+++ data.py
@@ -0,0 +1,13 @@
+import functions
+
+
+# Bajar informacion de .csv's donde se encuentran las ponderaciones iniciales y los activos
+path = 'files/NAFTRAC_2020_2022'
+df = functions.get_data(path)
+
+# Bajar precios y rendimientos
+start_date = "31-01-2020"
+end_date = "29-07-2022"
+precios = functions.portfolio_history(df, start_date, end_date) # Bajar precios
+rendimientos = precios.pct_change().dropna() # Obtener rendimientos
+
