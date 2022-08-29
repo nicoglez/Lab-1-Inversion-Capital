65f83bc2a0bf44156137f8a4f58c0dd0395097dc
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-08-29 11:41:06 -0500

    Simulacion Pasiva Data Frame

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import functions
 
-
 # Bajar informacion de .csv's donde se encuentran las ponderaciones iniciales y los activos
 path = 'files/NAFTRAC_2020_2022'
 df = functions.get_data(path)
@@ -8,6 +7,13 @@ df = functions.get_data(path)
 # Bajar precios y rendimientos
 start_date = "31-01-2020"
 end_date = "29-07-2022"
-precios = functions.portfolio_history(df, start_date, end_date) # Bajar precios
-rendimientos = precios.pct_change().dropna() # Obtener rendimientos
+precios = functions.portfolio_history(df, start_date, end_date)  # Bajar precios
+rendimientos = precios.pct_change().dropna()  # Obtener rendimientos
 
+# Definir clase de inversion pasiva
+capital = 1000000
+pasiva = functions.inversion_pasiva(df, precios, rendimientos, capital, start_date, end_date)
+
+# Simular inversion pasiva
+comision = 0.00125
+df_pasiva = pasiva.simulation(comision=comision)
