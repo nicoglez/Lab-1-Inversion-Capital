5db934100f794416ebf9f250c87619c28ffbc18e
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-09-03 11:57:25 -0500

    Update data.py

--- data.py
+++ data.py
@@ -9,11 +9,3 @@ start_date = "31-01-2020"
 end_date = "29-07-2022"
 precios = functions.portfolio_history(df, start_date, end_date)  # Bajar precios
 rendimientos = precios.pct_change().dropna()  # Obtener rendimientos
-
-# Definir clase de inversion pasiva
-capital = 1000000
-pasiva = functions.inversion_pasiva(df, precios, rendimientos, capital, start_date, end_date)
-
-# Simular inversion pasiva
-comision = 0.00125
-df_pasiva = pasiva.simulation(comision=comision)
